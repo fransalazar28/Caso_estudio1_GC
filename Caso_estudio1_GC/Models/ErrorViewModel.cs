fa6c851cd4770964ed98d4d38df486f681287100
mewmi <mewmi@DESKTOP-AK940H4>
2024-06-19 18:56:06 -0600

    Agregar archivos de proyecto.

---
+++ Caso_estudio1_GC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Caso_estudio1_GC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
